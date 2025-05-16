d60589d8bcfe093e01820971d89ed7c2ba1d3f63
李浩霖 -gregPerlinLi- <lihaolin13@outlook.com>
2025-05-16 23:15:54 +0800

    恢复原版

--- backend/env.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Content-Type: application/javascript');
-// 从环境变量中读取
-$ipv4 = getenv('IPV4_URL') ?: '';
-$ipv6 = getenv('IPV6_URL') ?: '';
-// 输出成 JS 全局变量
-echo 'window.IPV4_URL = ' . json_encode($ipv4) . ";\n";
-echo 'window.IPV6_URL = ' . json_encode($ipv6) . ";\n";
